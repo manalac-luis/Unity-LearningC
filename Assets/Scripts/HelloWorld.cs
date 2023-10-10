450fcadddaf932e968fd8e2fa3717282fa159afd
luis-trippinc <luis@trippinc.com>
2023-10-10 13:51:03 -0700

    09. Coroutines

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("LateUpdate");
+    }
+
+    private void OnGUI()
+    {
+        Debug.Log("OnGUI");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable");
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start");
+        // InvokeRepeating("myFunction", 1, 1);
+        StartCoroutine(TestRoutine());
     }
+
+    IEnumerator TestRoutine()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Debug.Log("TestRoutine");
+    }
+    /*
+    void myFunction()
+    {
+        Debug.Log("myFunction");
+    }
+    */
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Update");
     }
 }
